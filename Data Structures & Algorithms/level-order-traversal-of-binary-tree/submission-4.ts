ac6edaa1d8a82729902edc3445bcd7d85346391d
Aziz Abada <abadaa@users.noreply.github.com>
2026-08-01 15:51:13 -0700

    Add: level-order-traversal-of-binary-tree - submission-4

---
+++ Data Structures & Algorithms/level-order-traversal-of-binary-tree/submission-4.ts
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number[][]}
+     */
+    levelOrder(root: TreeNode | null): number[][] {
+        let queue: TreeNode[] = []
+        if(root) queue.push(root)
+        let result: number[][] = []
+        while(queue.length > 0) {
+            let levelLength = queue.length
+            let temp: number[] = []
+            for(let i = 0; i < levelLength; i++) {
+                const current: TreeNode = queue.shift()
+                temp.push(current.val)
+                if(current.left) queue.push(current.left)
+                if(current.right) queue.push(current.right)
+            }
+            result.push(temp)
+            temp = []
+        }
+
+        return result
+    }
+}
