5f662427699df55c7b4524700858b003be6de487
Aziz Abada <abadaa@users.noreply.github.com>
2026-08-04 15:51:57 -0700

    Add: lru-cache - submission-0

---
+++ Data Structures & Algorithms/lru-cache/submission-0.ts
@@ -0,0 +1,42 @@
+class LRUCache {
+    /**
+     * @param {number} capacity
+     */
+    private capacity: number
+    private cache: Map<number, number>
+
+    constructor(capacity: number) {
+        this.capacity = capacity
+        this.cache = new Map<number, number>()
+    }
+
+    /**
+     * @param {number} key
+     * @return {number}
+     */
+    get(key: number): number {
+        if(!this.cache.get(key)) return -1
+        const val = this.cache.get(key) 
+        this.cache.delete(key)
+        this.cache.set(key, val)
+        return val
+    }
+
+    /**
+     * @param {number} key
+     * @param {number} value
+     * @return {void}
+     */
+    put(key: number, value: number): void {
+        if(this.capacity === this.cache.size) {
+            if(this.cache.has(key)) {
+                this.cache.delete(key)
+                this.cache.set(key, value)
+            } else {
+                const first = this.cache.keys().next().value
+                this.cache.delete(first)
+            }
+        }
+        this.cache.set(key, value)
+    }
+}
