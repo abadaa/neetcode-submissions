06ee020ad5a9b9bb2ed45eaf95e37181fcd89292
Aziz Abada <abadaa@users.noreply.github.com>
2026-08-12 13:35:20 -0700

    Add: binary-tree-right-side-view - submission-3

---
+++ Data Structures & Algorithms/binary-tree-right-side-view/submission-3.ts
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number[]}
+     */
+    rightSideView(root: TreeNode | null): number[] {
+        //boilerplate
+        const queue = new Queue()
+        if(root) queue.enqueue(root)
+        let result: number[] = []
+        while(!queue.isEmpty()) {
+            let queueLength: number = queue.size()
+            let temp: number = 0
+            for(let i = 0; i < queueLength; i++) {
+                const curr = queue.dequeue()
+                if(i === queueLength - 1) temp = curr.val
+                if(curr.left) queue.enqueue(curr.left)
+                if(curr.right) queue.enqueue(curr.right)
+            }
+            result.push(temp)
+            temp = 0
+        }
+
+        return result
+    }
+}
